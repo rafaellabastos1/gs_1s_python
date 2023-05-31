d04c591472717d5a1d7d7a8f66d947c8bcf31f70
rafaellabastos1 <rm552425@fiap.com.br>
2023-05-31 09:48:27 -0300

    fiz o menu

--- python.py
+++ python.py
@@ -1,1 +1,63 @@
-python
+#menu
+print('Seja bem vindo à PlanTech! No que podemos te ajudar?'
+      '\n'
+      '\n1. Desejo saber mais sobre a missão da PlanTech'
+      '\n2. Desejo saber sobre o funcionamento do drone'
+      '\n3. Quero comprar um drone para detectar pragas'
+      '\n4. Identifiquei uma praga na plantação. O que faço agora?'
+      '\n5. Quem faz parte do projeto PlanTech?'
+      '\n6. Quero indicar um tratamento para uma praga'
+      '\n7. Feedback'
+      '\n8. Outro')
+
+#Opção 1 do menu: desejo saber mais sobre a missão da PlanTech ---- Colocar o primeiro e o último parágrafo do
+#   arquivo "Descrição geral da solução 2"
+
+
+
+#Opção 2 do menu: desejo saber sobre o funcionamenento do drone ---- Colocar o segundo parágrafo do arquivo
+#   "Descrição geral da solução 2"
+
+
+
+#Opção 3 do menu: quero comprar um drone para detectar pragas ---- 1º: Falar que a PlanTech ainda é um projeto e, por 
+#   isso, ainda não foi desenvolvido drones para a venda; Perguntar se o usuário deseja fazer uma simulação de como
+#   seria essa etapa. Se não: dizer obrigada e retornar ao menu; 
+#   Se sim 2º: fazer cadastro do cliente com os dados (nome completo, RG ou CNH, CPF ou CNPJ, e-mail, telefone, rua,
+#   número de residência, se precisar de complemento, país, estado, cidade, bairro, CEP); Sistema deve validar todos
+#   esses dados. 
+#   3º: mostrar os dados informados pelo cliente e perguntar se eles estão corretos; 
+#   4º: perguntar quantos drones o usuário deseja comprar; 
+#   5º: dizer obrigada e que x drones serão entregues em até 15 dias no endereço mencionado; fazer uma observação 
+#   para dizer também que, como foi dito, este era apenas um teste e que, por isso, não foi pedido dados de
+#   pagamento e nem os drones serão entregues de fato ao endereço mencionado.
+
+
+
+#Opção 4 do menu: identifiquei uma praga na plantação. O que faço agora? ---- Fazer um sistema com x tratamentos 
+#   para pragas e x tratamentos que x plantações pode receber. A partir disso, fazer todas as combinações possíveis 
+#   em comum. O usuário deve ter a opção de digitar o nome da praga e o nome da plantação atacada; Se colocar uma
+#   praga ou plantação que não tenha no sistema, dizer que ainda não há esse no sistema
+
+
+
+#Opção 5 do menu: quem faz parte do projeto PlanTech? ---- Colocar o nome e RM de todos do grupo
+
+
+
+#Opção 6 do menu: quero indicar o tratamento para uma praga ---- Deixar com que o usuário informe o nome de uma 
+#   praga, o tipo de plantação que essa praga ataca e o tipo de tratamento que essa praga combinada com essa 
+#   plantação pode receber; Mostrar uma mensagem agradecendo e dizendo que essas informações vão para análise e
+#   se estiver tudo correto, serão incluídas no sistema PlanTech
+
+
+
+#Opção 7 do menu: feedback ---- o usuário pode dar um feedback sobre a resolução de suas dúvidas, da performace do
+#   sistema, sobre a indicação dos tratamentos para as plantações, sobre a usabilidade do projeto
+
+
+
+#Opção 8 do menu: outro ---- Colocar pequenas informações e onde pode encontrá-las: se quer tirar dúvidas mais
+#   específicas, entrar no nosso ChatBot; Se quer textos mais explicativos como agricultura sustentável, uso de
+#   IA's generativas na produção, distribuição de alimentos pelo mundo, modelos de cultivos eficientes e calendário
+#   de controle de pragas, entrar no site da PlanTech
