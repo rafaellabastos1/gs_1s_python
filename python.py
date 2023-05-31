f3b998ce9fd0fca637ce1c589039b0041d4306ce
rafaellabastos1 <rm552425@fiap.com.br>
2023-05-31 20:56:28 -0300

    criei as variáveis das funções pragas, tratamentos e plantações; criei um switch case pra poder fazer testes

--- python.py
+++ python.py
@@ -1,4 +1,42 @@
+def Tratamentos():
+      spodovir = ''
+      engeoPlenoS = ''
+      bacillus = ''
+      aug = ''
+
+def Plantacao():
+      algodao = ''
+      arroz = ''
+      aveia = ''
+      cafe = ''
+      cana = ''
+      feijao = ''
+      girassol = ''
+      milho = ''
+      milhoSafrinha = ''
+      soja = ''
+      trigo = ''
+
+
+def Pragas():
+      cigarrinhaMilho = ''
+      cupimMonticulo = ''
+      lagartaCartucho = ''
+      lagartaHelicoverpa = ''
+      percevejoMarrom = ''
+      percevejoVerdePeq = ''
+      percevejoSoja = ''
+      percevejoArroz = ''
+      percevejoBarrigaVerde = ''
+      phyllophaga = ''
+      pulgao = ''
+      pulgaoAlgodoeiro = ''
+      tripes = ''
+      vaquinhaVerdeAmarela = ''
+
+
 #menu
+opcaoMenu = 0
 print('Seja bem vindo à PlanTech! No que podemos te ajudar?'
       '\n'
       '\n1. Desejo saber mais sobre a missão da PlanTech' #Luiz
@@ -9,16 +47,18 @@ print('Seja bem vindo à PlanTech! No que podemos te ajudar?'
       '\n6. Quero indicar um tratamento para uma praga' #Gustavo
       '\n7. Feedback' #Douglas
       '\n8. Outro') #Rafa
+opcaoMenu = int(input('Selecione uma das opções acima: '))
 
+match opcaoMenu:
 #Opção 1 do menu: desejo saber mais sobre a missão da PlanTech ---- Colocar o primeiro e o último parágrafo do
 #   arquivo "Descrição geral da solução 2"
-
-
+      case 1:
+            print("-")
 
 #Opção 2 do menu: desejo saber sobre o funcionamenento do drone ---- Colocar o segundo parágrafo do arquivo
 #   "Descrição geral da solução 2"
-
-
+      case 2:
+            print("-")
 
 #Opção 3 do menu: quero comprar um drone para detectar pragas ---- 1º: Falar que a PlanTech ainda é um projeto e, por 
 #   isso, ainda não foi desenvolvido drones para a venda; Perguntar se o usuário deseja fazer uma simulação de como
@@ -31,30 +71,37 @@ print('Seja bem vindo à PlanTech! No que podemos te ajudar?'
 #   5º: dizer obrigada e que x drones serão entregues em até 15 dias no endereço mencionado; fazer uma observação 
 #   para dizer também que, como foi dito, este era apenas um teste e que, por isso, não foi pedido dados de
 #   pagamento e nem os drones serão entregues de fato ao endereço mencionado.
-
-
+      case 3:
+            print("-")
 
 #Opção 4 do menu: identifiquei uma praga na plantação. O que faço agora? ---- Fazer um sistema com x tratamentos 
 #   para pragas e x tratamentos que x plantações pode receber. A partir disso, fazer todas as combinações possíveis 
 #   em comum. O usuário deve ter a opção de digitar o nome da praga e o nome da plantação atacada; Se colocar uma
 #   praga ou plantação que não tenha no sistema, dizer que ainda não há esse no sistema
-
+      case 4:
+            print("-")
 
 
 #Opção 5 do menu: quem faz parte do projeto PlanTech? ---- Colocar o nome e RM de todos do grupo
-
+      case 5:
+            print("-")
 
 
 #Opção 6 do menu: quero indicar o tratamento para uma praga ---- Deixar com que o usuário informe o nome de uma 
 #   praga, o tipo de plantação que essa praga ataca e o tipo de tratamento que essa praga combinada com essa 
 #   plantação pode receber; Mostrar uma mensagem agradecendo e dizendo que essas informações vão para análise e
 #   se estiver tudo correto, serão incluídas no sistema PlanTech
+      case 6:
+            print("-")
+
 
 
 
 #Opção 7 do menu: feedback ---- o usuário pode dar um feedback sobre a resolução de suas dúvidas, da performace do
 #   sistema, sobre a indicação dos tratamentos para as plantações, sobre a usabilidade do projeto e experiência com 
 #   o site
+      case 7:
+            print("-")
 
 
 
@@ -62,3 +109,5 @@ print('Seja bem vindo à PlanTech! No que podemos te ajudar?'
 #   específicas, entrar no nosso ChatBot; Se quer textos mais explicativos como agricultura sustentável, uso de
 #   IA's generativas na produção, distribuição de alimentos pelo mundo, modelos de cultivos eficientes e calendário
 #   de controle de pragas, entrar no site da PlanTech
+      case 8:
+            print("-")
