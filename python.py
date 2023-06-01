0c26f80da4b836e0ffe390630b7284f7521c67c7
rafaellabastos1 <rm552425@fiap.com.br>
2023-05-31 22:06:57 -0300

    acrescentei um while para o programa sempre retornar para o menu

--- python.py
+++ python.py
@@ -43,7 +43,7 @@ def Indicacao():
                   print('Faça o tratamento d')
             elif nomePraga == 'Tripes' or 'tripes':
                   print('Faça o tratamento e')
-            elif nomePraga == 'Cupim do montículo' or 'Cupim do monticulo' or 'cupim do montículo' or 'cupim do monticulo' or 'Cupim do Montículo' or 'Cupim do Monticulo'       
+            elif nomePraga == 'Cupim do montículo' or 'Cupim do monticulo' or 'cupim do montículo' or 'cupim do monticulo' or 'Cupim do Montículo' or 'Cupim do Monticulo':       
                   print('Faça o tratamento f')
       
       elif nomePlanta == 'arroz' or 'Arroz':
@@ -112,31 +112,32 @@ def Indicacao():
 
 
 
-
 #menu
 opcaoMenu = 0
-print('Seja bem vindo à PlanTech! No que podemos te ajudar?'
-      '\n'
-      '\n1. Desejo saber mais sobre a missão da PlanTech' #Luiz
-      '\n2. Desejo saber sobre o funcionamento do drone' #Igor
-      '\n3. Quero comprar um drone para detectar pragas' #Luiz
-      '\n4. Identifiquei uma praga na plantação. O que faço agora?' #Rafa
-      '\n5. Quem faz parte do projeto PlanTech?' #Douglas
-      '\n6. Quero indicar um tratamento para uma praga' #Gustavo
-      '\n7. Feedback' #Douglas
-      '\n8. Outro') #Rafa
-opcaoMenu = int(input('Selecione uma das opções acima: '))
+while opcaoMenu != 9:
+      print('Seja bem vindo à PlanTech! No que podemos te ajudar?'
+            '\n'
+            '\n1. Desejo saber mais sobre a missão da PlanTech' #Luiz
+            '\n2. Desejo saber sobre o funcionamento do drone' #Igor
+            '\n3. Quero comprar um drone para detectar pragas' #Luiz
+            '\n4. Identifiquei uma praga na plantação. O que faço agora?' #Rafa
+            '\n5. Quem faz parte do projeto PlanTech?' #Douglas
+            '\n6. Quero indicar um tratamento para uma praga' #Gustavo
+            '\n7. Feedback' #Douglas
+            '\n8. Outro' #Rafa
+            '\n9. Encerrar')
+      opcaoMenu = int(input('Selecione uma das opções acima: '))
 
-match opcaoMenu:
+      match opcaoMenu:
 #Opção 1 do menu: desejo saber mais sobre a missão da PlanTech ---- Colocar o primeiro e o último parágrafo do
 #   arquivo "Descrição geral da solução 2"
-      case 1:
-            print("-")
+            case 1:
+                  print("-")
 
 #Opção 2 do menu: desejo saber sobre o funcionamenento do drone ---- Colocar o segundo parágrafo do arquivo
 #   "Descrição geral da solução 2"
-      case 2:
-            print("-")
+            case 2:
+                  print("-")
 
 #Opção 3 do menu: quero comprar um drone para detectar pragas ---- 1º: Falar que a PlanTech ainda é um projeto e, por 
 #   isso, ainda não foi desenvolvido drones para a venda; Perguntar se o usuário deseja fazer uma simulação de como
@@ -149,28 +150,28 @@ match opcaoMenu:
 #   5º: dizer obrigada e que x drones serão entregues em até 15 dias no endereço mencionado; fazer uma observação 
 #   para dizer também que, como foi dito, este era apenas um teste e que, por isso, não foi pedido dados de
 #   pagamento e nem os drones serão entregues de fato ao endereço mencionado.
-      case 3:
-            print("-")
+            case 3:
+                  print("-")
 
 #Opção 4 do menu: identifiquei uma praga na plantação. O que faço agora? ---- Fazer um sistema com x tratamentos 
 #   para pragas e x tratamentos que x plantações pode receber. A partir disso, fazer todas as combinações possíveis 
 #   em comum. O usuário deve ter a opção de digitar o nome da praga e o nome da plantação atacada; Se colocar uma
 #   praga ou plantação que não tenha no sistema, dizer que ainda não há esse no sistema
-      case 4:
-            print(Indicacao())
+            case 4:
+                  print(Indicacao())
 
 
 #Opção 5 do menu: quem faz parte do projeto PlanTech? ---- Colocar o nome e RM de todos do grupo
-      case 5:
-            print("-")
+            case 5:
+                  print("-")
 
 
 #Opção 6 do menu: quero indicar o tratamento para uma praga ---- Deixar com que o usuário informe o nome de uma 
 #   praga, o tipo de plantação que essa praga ataca e o tipo de tratamento que essa praga combinada com essa 
 #   plantação pode receber; Mostrar uma mensagem agradecendo e dizendo que essas informações vão para análise e
 #   se estiver tudo correto, serão incluídas no sistema PlanTech
-      case 6:
-            print("-")
+            case 6:
+                  print("-")
 
 
 
@@ -178,8 +179,8 @@ match opcaoMenu:
 #Opção 7 do menu: feedback ---- o usuário pode dar um feedback sobre a resolução de suas dúvidas, da performace do
 #   sistema, sobre a indicação dos tratamentos para as plantações, sobre a usabilidade do projeto e experiência com 
 #   o site
-      case 7:
-            print("-")
+            case 7:
+                  print("-")
 
 
 
@@ -187,5 +188,5 @@ match opcaoMenu:
 #   específicas, entrar no nosso ChatBot; Se quer textos mais explicativos como agricultura sustentável, uso de
 #   IA's generativas na produção, distribuição de alimentos pelo mundo, modelos de cultivos eficientes e calendário
 #   de controle de pragas, entrar no site da PlanTech.
-      case 8:
-            print("-")
+            case 8:
+                  print("-")
