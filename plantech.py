826f698d851fa2c9ecd85e2601dcbcf3f3814c29
igoranccilotto <rm550415@fiap.com.br>
2023-06-02 13:31:04 -0300

    validação concluida

--- plantech.py
+++ plantech.py
@@ -320,7 +320,15 @@ while opcaoMenu != 9:
 #Opção 2 do menu: desejo saber sobre o funcionamenento do drone ---- Colocar o segundo parágrafo do arquivo
 #   "Descrição geral da solução 2"
       if opcaoMenu == 2:
-            print("-")
+            print("\n----PlanTech ----"
+                        +"\n Para isso, será desenvolvido um drone robô  drone robô que detecta pragas nas plantações e informa suas localidades para o combate."
+                        +"Essa detecção é por meio de uma IA generativa que identifica as pragas nas plantações com infravermelho ainda na fase inicial e informa ao dono da plantação o nome da praga e o seu local no visor do drone."
+                        +"Com essa informação, o dono da plantação entra no sistema da PlanTech e informa o nome da praga e qual plantação ela está atacando para que, dessa forma, seja indicado um tratamento saudável e sustentável para que aquela plantação não seja perdida, danificada e nem tenha alastramento das pragas."
+                        +"Com essa informação, o dono da plantação entra no sistema da PlanTech e informa o nome da praga e qual plantação ela está atacando para que, dessa forma, seja indicado um tratamento saudável e sustentável para que aquela plantação não seja perdida, danificada e nem tenha alastramento das pragas. "
+                        +"Este drone funciona sozinho e é recarregável, sendo necessário somente ligar ele para que ele sobrevoe a plantação em busca de possíveis pragas."
+                        +"O usuário pode também entrar em contato com o nosso ChatBot para solucionar possíveis dúvidas, dar seu feedback e entrar no site da PlanTech para mais informações sobre agricultura ou semelhantes. "
+                        +"Além disso, o agricultor pode contribuir com informações de pragas e tratamentos que não tenham no site.")
+
 
 
 #Opção 3 do menu: quero comprar um drone para detectar pragas 
